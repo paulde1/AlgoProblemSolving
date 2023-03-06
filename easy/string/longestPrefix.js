657b9afa3e43a001471b37a720ef1eb1d027c44d
Paul De Cunha <pauldecunha7117@gmail.com>
2023-03-06 13:09:43 -0500

    longest prefix

---
+++ easy/string/longestPrefix.js
@@ -0,0 +1,9 @@
+const longestCommonPrefix = function (strs) {
+  strs.sort();
+  for (let i = 0; i < strs[0].length; i++) {
+    if (strs[0][i] !== strs[strs.length - 1][i]) {
+      return strs[0].substring(0, i);
+    }
+  }
+  return strs[0];
+};
