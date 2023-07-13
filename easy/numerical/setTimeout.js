4b2ee3cb8012d3553f7950937dc65042ceb5ae54
Paul De Cunha <pauldecunha7117@gmail.com>
2023-07-13 17:47:16 -0400

    proper set timeout

--- easy/numerical/setTimeout.js
+++ easy/numerical/setTimeout.js
@@ -5,3 +5,12 @@
 //             } ,millis)
 //     }) 
 // }
+
+async function sleep(millis) {
+    await new Promise (resolve => setTimeout(resolve, millis))
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
